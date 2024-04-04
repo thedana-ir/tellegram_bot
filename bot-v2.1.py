73b65b0a216640ed01e663a1235ff2a5b6815c34
Mohsen Yazdandust <m.yazdandust1403@gmail.com>
2024-04-04 05:19:55 +0330

    ya ali

--- bot-v2.1.py
+++ bot-v2.1.py
@@ -1,204 +1,76 @@
-from pyrogram import Client,filters
-from pyrogram.types import User, Message,ReplyKeyboardMarkup
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
-import redis
+from pyrogram import Client, filters
+from pyrogram.types import ReplyKeyboardMarkup, InlineKeyboardButton, InlineKeyboardMarkup
+import os
 import requests
 import json
 
-
-# tellegrom authorization 
-# must be in invirment variable!!!!!!!!!!!!!!!!!!!!!!!!!!
-
+# Telegram authorization
 api_id = 1500016
 api_hash = "e07225f6d40b6208ea372b72c613d04c"
 bot_token = "6541504105:AAHsRnVkr8pbbbCkXEmvdQXqPsG01Po03gE"
 proxy = None
 
-
-# redis database for level partitioning
-
-bot_database = redis.Redis(host='localhost', port=6379, decode_responses=True)
-
-# bot object
-
+# Bot object
 bot = Client(
     "hiboo",
-    api_id=api_id, api_hash=api_hash,
+    api_id=api_id,
+    api_hash=api_hash,
     bot_token=bot_token,
     proxy=proxy,
 )
 
-# section 1: private chats
- 
-#   part 1: chat creation & deffenitions 
+# Function to create reply keyboard buttons
+def reply_keyboard_button(text, buttons, one_time_being):
+    return ReplyKeyboardMarkup(
+        buttons,
+        one_time_keyboard=one_time_being,
+        resize_keyboard=True
+    )
 
-@bot.on_message(filters.command('start') & filters.private)
-def start(bot, message):
-    global reply_keyboard_buttom, inline_keyboard_buttoms, back_to_menu
+# Function to create inline keyboard buttons
+def inline_keyboard_buttons(text, buttons):
+    return InlineKeyboardMarkup(buttons)
 
-    # keyboard buttoms creater
-    def reply_keyboard_buttom(text, buttoms, one_time_being):
+# Function to send message with inline keyboard
+def send_message_with_inline_keyboard(bot, chat_id, text, buttons):
+    bot.send_message(
+        chat_id=chat_id,
+        text=text,
+        reply_markup=inline_keyboard_buttons(text, buttons)
+    )
 
-        message.reply(
-            text=text,
-            reply_markup = ReplyKeyboardMarkup(
-                buttoms,
-                one_time_keyboard=one_time_being,
-                resize_keyboard=True
-                )
-        )
-
-    # inline buttoms creater
-    def inline_keyboard_buttoms(text, buttoms):
-
-        a = message.reply(
-            text = text,
-            reply_markup = InlineKeyboardMarkup(buttoms)
-        )
-
-        return a
-        
-
-    # buttom in keyboard for return to home
-    def back_to_menu(text):
-
-        bottoms = [
-            [
-            ('برگرد به منو'),
-            ]
-        ]
-
-        reply_keyboard_buttom(text, bottoms)
-        bot_database.set(message.chat.id, 'start')
-
-    # def send_inline_buttoms()
-
-    # hello message and menu
-    user_name = message.chat.username
-    text = f'سلام {user_name} عزیز 🖐🏻' + '\n \n اسم جزوه ای که میخوای رو بهم بگو 🤓 \n یا حتی اسم استادش :)😲 \n \n از منو پایین هم میتونی به قسمت های دیگه ربات دسترسی پیدا کنید😊 \n \n هیبو | دستیار آموزشی تو🦉'
-    buttoms = [
-        [
-            ('میخوام جزوه آپلود کنم')
-        ],
-        [
-            ('راهنما ربات'), ('هیبو چیه')
-        ]
-    ]
-    reply_keyboard_buttom(text, buttoms,False)
-
-
-#   part 2: find word in website & response to keyboard buttoms
-    
-@bot.on_message(filters.private)
-def body(bot, message):
-
-    level = message.text
-    # answer to buttoms
-    if level == 'میخوام جزوه آپلود کنم':
-
-        bot.send_message(message.chat.id, 'متاسفانه این قابلیت هنوز تکمیل نشده \n بزودی این امکان هم به ربات اضافه میشه 😀')
-
-    elif level == 'راهنما ربات':
-
-        bot.send_message(message.chat.id, 'هیبو بات هستم 🤖\n\nهر جای چت که هستی میتونی اسم درست یا استادی که میخوای رو برام بنویسی 🤓\nمن برات میگردم و همه نتایجی که پیدا کردم رو نشون میدم😉\nهمینطور از دکمه های منو میتونی هم منابع درسی خودت رو آپلود کنی هم به وبسایت وصل بشی💯\n\nامیدوارم تونسته باشم خوب راهنماییت کنم✌️')
-    
-    elif level == 'هیبو چیه':
-        
-        bot.send_message(message.chat.id, 'اینو محسن بگه میگم بت')
-
-    # serach for data in website
-    else:
-        
-        # first message to tell client please wait
-        sent_message = bot.send_message(message.chat.id, 'صبر کن ببینم چی داریم 🤔')
-
-        # request to website backend
-        wanted_word = message.text
-        website_response = requests.get(f"https://hiboo.ir/api/v1/search/?s={wanted_word}")
-
-        global final_response
-        final_response = json.loads(website_response.text)
-
-        # delete sent message
-        bot.delete_messages(message.chat.id, sent_message.id)
-
-        # condition for if there wasn't any data to return
-        if len(final_response) == 0:
-
-            bot.send_message(message.chat.id, 'متاسفانه چیزی پیدا نکردم!🥲\nبا کلمه کلیدی دیگه ای در موردش امتحان کن\n\nمیتونی اگه جزوه یا منبع درسی ای تو این موضوع پیدا کردی با آپلودش توی سایت به بقیه کمک کنی❤️')
-        
-        # show found result in inline buttoms
-        else:
-        
-            bot.send_message(message.chat.id, f'🟡 این {len(final_response)} تا رو پیدا کردم :👇')
-
-            global number_of_sent_response
-            number_of_sent_response = 0
-            for i in final_response:
-
-                if number_of_sent_response <= 4:
-
-                    buttom = []
-                    text = '🟣' + str(i['title']) + ' | ' + str(i['teacher']['name'])
-
-                    buttom.append([InlineKeyboardButton('دیدن جزئیات', callback_data = str(number_of_sent_response))])
-                    number_of_sent_response += 1
-
-                    inline_keyboard_buttoms(text, buttom)
-
-                else:
-
-                    more_buttom = [
-                        [InlineKeyboardButton('نتایج بیشتر ...', callback_data = 'more_data')]
-                    ]
-                    more_text = 'نتایج بیشتری هم هست 👇'
-                    global more_data_details
-                    more_data_details = inline_keyboard_buttoms(more_text, more_buttom)
-                    break
-
-#   part 3: response to choosen inline buttom 
+# Function to handle callback queries
 @bot.on_callback_query()
-def response_to_inlinebuttoms(Client, CallbackQuery):
-    global number_of_sent_response, final_response, more_data_details
+def response_to_inline_buttons(bot, callback_query):
+    choosen_one = callback_query.data
+    final_response = callback_query.message.reply_to_message.final_response
     
-    # set choosen_one for data returned of buttoms
-    choosen_one = CallbackQuery.data
-    
-    # button of show more results
     if choosen_one == 'more_data':
+        number_of_sent_response = callback_query.message.reply_to_message.number_of_sent_response
+        more_data_details = callback_query.message.reply_to_message.more_data_details
+        bot.delete_messages(callback_query.from_user.id, more_data_details.id)
+        for i in final_response[int(number_of_sent_response):]:
+            buttoms = []
+            text = '🟣' + str(i['title']) + ' | ' + str(i['teacher']['name'])
 
-        second_counter = 0
-        for i in final_response:
-            
-            bot.delete_messages(CallbackQuery.from_user.id, more_data_details.id)
-            if second_counter == number_of_sent_response:
+            buttoms.append([InlineKeyboardButton('دیدن جزئیات', callback_data = str(number_of_sent_response))])
+            number_of_sent_response += 1
 
-                buttoms = []
-                text = '🟣' + str(i['title']) + ' | ' + str(i['teacher']['name'])
-
-                buttoms.append([InlineKeyboardButton('دیدن جزئیات', callback_data = str(number_of_sent_response))])
-                number_of_sent_response += 1
-
-                inline_keyboard_buttoms(text, buttoms)
-
-            second_counter += 1
-
-    # buttons of showed results
+            inline_keyboard_buttons(text, buttoms)
     else:
-        
         # create caption to send with preview photo
-        caption = '🟠 ' + final_response[int(CallbackQuery.data)]['title']
-        caption += '\n' + '👨🏻‍🏫 نام استاد: ' + final_response[int(CallbackQuery.data)]['teacher']['name']
-        caption += '\n' + '🎒 دانشگاه: ' + final_response[int(CallbackQuery.data)]['university']['name']
+        caption = '🟠 ' + final_response[int(callback_query.data)]['title']
+        caption += '\n' + '👨🏻‍🏫 نام استاد: ' + final_response[int(callback_query.data)]['teacher']['name']
+        caption += '\n' + '🎒 دانشگاه: ' + final_response[int(callback_query.data)]['university']['name']
 
         # send result with preview, caption and a button to download
         bot.send_photo(
 
             # where to send
-            CallbackQuery.from_user.id,
+            callback_query.from_user.id,
             
             # photo 
-            final_response[int(CallbackQuery.data)]['preview']['file'],
+            final_response[int(callback_query.data)]['preview']['file'],
 
             # caption
             caption = caption,
@@ -207,11 +79,63 @@ def response_to_inlinebuttoms(Client, CallbackQuery):
             reply_markup = InlineKeyboardMarkup(
                 [
                     [InlineKeyboardButton('دانلود',
-                    url=f'https://hiboo.ir/hub/sources/{final_response[int(CallbackQuery.data)]["slug"]}/download/')]
+                    url=f'https://hiboo.ir/hub/sources/{final_response[int(callback_query.data)]["slug"]}/download/')]
                 ]
             )
         )
 
+# Function to handle private messages
+@bot.on_message(filters.private & filters.command('start'))
+def start(bot, message):
+    user_name = message.chat.username
+    text = f'سلام {user_name} عزیز 🖐🏻\n\n اسم جزوه ای که میخوای رو بهم بگو 🤓 \n یا حتی اسم استادش :)😲 \n\n هیبو | دستیار آموزشی تو🦉'
+    buttons = [
+        [('میخوام جزوه آپلود کنم')],
+        [('راهنما ربات'), ('هیبو چیه')]
+    ]
+    bot.send_message(
+        chat_id=message.chat.id,
+        text=text,
+        reply_markup=reply_keyboard_button(text, buttons, False)
+    )
 
-# lets run:)
+@bot.on_message(filters.private)
+def body(bot, message):
+    level = message.text
+    if level == 'میخوام جزوه آپلود کنم':
+        bot.send_message(message.chat.id, 'متاسفانه این قابلیت هنوز تکمیل نشده \n بزودی این امکان هم به ربات اضافه میشه 😀')
+    elif level == 'راهنما ربات':
+        bot.send_message(message.chat.id, 'هیبو بات هستم 🤖\n\nهر جای چت که هستی میتونی اسم درست یا استادی که میخوای رو برام بنویسی 🤓\nمن برات میگردم و همه نتایجی که پیدا کردم رو نشون میدم😉\nهمینطور از دکمه های منو میتونی هم منابع درسی خودت رو آپلود کنی هم به وبسایت وصل بشی💯\n\nامیدوارم تونسته باشم خوب راهنماییت کنم✌️')
+    elif level == 'هیبو چیه':
+        bot.send_message(message.chat.id, 'اینو محسن بگه میگم بت')
+    else:
+        sent_message = bot.send_message(message.chat.id, 'صبر کن ببینم چی داریم 🤔')
+        wanted_word = message.text
+        website_response = requests.get(f"https://hiboo.ir/api/v1/search/?s={wanted_word}")
+        final_response = json.loads(website_response.text)
+
+        if len(final_response) == 0:
+            bot.send_message(message.chat.id, 'متاسفانه چیزی پیدا نکردم!🥲\nبا کلمه کلیدی دیگه ای در موردش امتحان کن\n\nمیتونی اگه جزوه یا منبع درسی ای تو این موضوع پیدا کردی با آپلودش توی سایت به بقیه کمک کنی❤️')
+        else:
+            sent_message.reply_to_message.final_response = final_response
+            
+            bot.send_message(message.chat.id, f'🟡 این {len(final_response)} تا رو پیدا کردم :👇')
+
+            number_of_sent_response = 0
+            for i in final_response:
+                if number_of_sent_response <= 4:
+                    text = '🟣' + str(i['title']) + ' | ' + str(i['teacher']['name'])
+                    buttons = [[InlineKeyboardButton('دیدن جزئیات', callback_data=str(number_of_sent_response))]]
+                    send_message_with_inline_keyboard(bot, message.chat.id, text, buttons)
+                    number_of_sent_response += 1
+                else:
+                    sent_message.reply_to_message.number_of_sent_response = number_of_sent_response
+                    more_text = 'نتایج بیشتری هم هست 👇'
+                    more_buttons = [[InlineKeyboardButton('نتایج بیشتر ...', callback_data='more_data')]]
+                    sent_message.reply_to_message.more_data_details = more_buttons
+                    
+                    send_message_with_inline_keyboard(bot, message.chat.id, more_text, more_buttons)
+                    break
+
+# Run the bot
 bot.run()
